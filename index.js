3c8f534d606478f7a026d50f8fa7922e162eaafb
alonso-14-14 <lukas_zimmermann_14@bluewin.ch>
2016-03-13 18:21:28 +0100

    update indendation, add readme

--- index.js
+++ index.js
@@ -5,84 +5,84 @@ import { Flex } from 'reflexbox'
 import React from 'react'
 
 const Counter = React.createClass({
-	getInitialState () {
-		return {
-			counter: this.props.init || 0,
-			incrementBy: 1,
-			checked: false
-		}
-	},
+  getInitialState () {
+    return {
+      counter: this.props.init || 0,
+      incrementBy: 1,
+      checked: false
+    }
+  },
 
-	onIncrement (e) {
-		this.state.checked
-			?	this.setState({ counter: this.state.counter + this.state.incrementBy })
-			: this.setState({ counter: this.state.counter - this.state.incrementBy })
-	},
+  onIncrement (e) {
+    this.state.checked
+      ? this.setState({ counter: this.state.counter + this.state.incrementBy })
+      : this.setState({ counter: this.state.counter - this.state.incrementBy })
+  },
 
-	onChange (e) {
-		console.log(e.target.value)
-		this.setState({ incrementBy: ~~e.target.value })
-	},
+  onChange (e) {
+    console.log(e.target.value)
+    this.setState({ incrementBy: ~~e.target.value })
+  },
 
-	onSwitch () {
-		this.setState({ checked: !this.state.checked })
-	},
+  onSwitch () {
+    this.setState({ checked: !this.state.checked })
+  },
 
-	render () {
-		return (
-			<Panel theme="info">
-				<Flex align='center'>
-					<Input
-					  label="Increment Count By"
-					  name="input_example"
-					  defaultValue={this.state.incrementBy}
-					  rounded={true}
-					  type="text"
-					  onChange={this.onChange}
-					/>
+  render () {
+    return (
+      <Panel theme="info">
+        <Flex align='center'>
+          <Input
+            label="Increment Count By"
+            name="input_example"
+            defaultValue={this.state.incrementBy}
+            rounded={true}
+            type="text"
+            onChange={this.onChange}
+          />
 
-					<Space x={2} />
+          <Space x={2} />
 
-					<Switch checked={this.state.checked} onClick={this.onSwitch} />
+          <Switch checked={this.state.checked} onClick={this.onSwitch} />
 
-					<Space x={2} />
+          <Space x={2} />
 
-					<button onClick={this.onIncrement}>
-						{ this.state.checked ? '+' : '-' }
-					</button>
-				</Flex>
-				<h1>{this.state.counter}</h1>
-			</Panel>
-		)
-	}
+          <button onClick={this.onIncrement}>
+            { this.state.checked ? '+' : '-' }
+          </button>
+        </Flex>
+        <h1>{this.state.counter}</h1>
+      </Panel>
+    )
+  }
 })
 
 const CounterList = React.createClass({
-	getInitialState () {
-		return {
-			counterList: [
-				0,
-				4,
-				2,
-				6
-			]
-		}
-	},
+  getInitialState () {
+    return {
+      counterList: [
+        0,
+        4,
+        2,
+        6
+      ]
+    }
+  },
 
-	render () {
-		return (
-			<div>
-				{
-					this.state.counterList.map((count, i) => (
-						<Counter key={i} init={count} />
-					))
-				}
-			</div>
-		)
-	}
+  render () {
+    return (
+      <div>
+        {
+          this.state.counterList.map((count, i) => (
+            <Counter key={i} init={count} />
+          ))
+        }
+      </div>
+    )
+  }
 })
 
 ReactDOM.render(
-	<CounterList />,
-	document.getElementById('root')
+  <CounterList />,
+  document.getElementById('root')
 )
---
+++ readme.md
@@ -0,0 +1,18 @@
+
+# react-counter-example
+
+[![forthebadge](http://forthebadge.com/images/badges/built-with-love.svg)](http://forthebadge.com)
+
+Small counter example using react.
+
+## Installation
+
+```bash
+> git clone https://github.com/alonso-14-14/react-counter-example.git
+> npm install
+> quick
+```
+
+### License
+
+MIT
